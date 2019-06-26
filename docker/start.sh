20144bb01b5520b5cf8b46d0e057256b164df830
jizhi <1103287597@qq.com>
2019-06-26 15:45:11 +0800

    Metis customized project modify first time, fixing docker start.sh to run metis in local containers!

--- docker/start.sh
+++ docker/start.sh
@@ -15,3 +15,10 @@ sleep 6
 docker run --net=host --name=metis-svr -d -p 8080:8080 -v /data/metis/model/:/metis/time_series_detector/model:Z zhiyunmetis/metis-svr /bin/sh /metis/init.sh
 docker run --net=host --name=metis-web -d -p 80:80 zhiyunmetis/metis-web /bin/sh /metis/init.sh ${ip}
 
+# modify by jizhi 20190626
+
+docker run --name=metis-db -d -p 3306:3306 -v /data/metis/mysql/:/var/lib/mysql:Z -e MYSQL_ROOT_PASSWORD=metis@123 zhiyunmetis/metis-db
+sleep 6
+docker run --name=metis-svr -d -p 8080:8080 -v /data/metis/model/:/metis/time_series_detector/model:Z zhiyunmetis/metis-svr /bin/sh /metis/init.sh
+docker run --name=metis-web -d -p 80:80 zhiyunmetis/metis-web /bin/sh /metis/init.sh ${ip}
+
